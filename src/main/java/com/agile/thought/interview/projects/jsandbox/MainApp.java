500c25d58c8fe4a1c3bf6fe83b0263a3479f3ce1
ptr.logo <ptr.lopezgomez@gmail.com>
2022-01-28 18:29:14 -0600

    Initial version

---
+++ src/main/java/com/agile/thought/interview/projects/jsandbox/MainApp.java
@@ -0,0 +1,14 @@
+package com.agile.thought.interview.projects.jsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MainApp.class, args);
+		String[] variable = {"12","12","12","12"};
+	}
+
+}
---
+++ src/test/java/com/agile/thought/interview/projects/jsandbox/CarWashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agile.thought.interview.projects.jsandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarWashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
